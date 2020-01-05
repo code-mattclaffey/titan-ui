72ee156ba1e686566b569090c49dac849faec3b1
Matthew Claffey <matthewclaffey@matthews-mbp.mynet>
2020-01-05 17:14:35 +0000

    📦- region styling & some amends to the heading component

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -4,13 +4,14 @@ import { HeadingInterface } from './types';
 
 export const Heading = ({
     as = 'h2',
+    type = 'h2',
     additionalClassNames = '',
     children,
     ...rest
 }: HeadingInterface) => {
     const HtmlEl = as;
     const classes = classNames(
-        `e-heading e-heading--${as}`,
+        `e-heading e-heading--${type}`,
         additionalClassNames,
     );
 
--- src/components/heading/types.ts
+++ src/components/heading/types.ts
@@ -2,4 +2,5 @@ import { GenericComponent } from '../../module';
 
 export interface HeadingInterface extends GenericComponent {
     as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
