df3046ff32746f84d4653ddeee2e7f1b65c4d9d4
Matt Claffey <mattclaffey93@gmail.com>
2020-03-30 15:33:38 +0100

    fix: :bug: jumped the gun with removing babel as jest needs it

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.export = {
         '\\.(ts|tsx)?$': 'ts-jest',
     },
     testMatch: ['<rootDir>/src/**/>(*.)test.{ts, tsx}'], // finds test
-    moduleFileExtensions: ['json', 'node', 'tsx', 'ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     setupFilesAfterEnv: [
         '@testing-library/jest-dom/extend-expect',
