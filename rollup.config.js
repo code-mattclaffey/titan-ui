f34d4c5606d74c0315206bc648ae1b2ccb3cb33d
Matt Claffey <mattclaffey93@gmail.com>
2020-03-30 14:02:13 +0100

    feat: add tree shaking with rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+export default [
+    {
+        input: './src/index.ts',
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+            },
+            {
+                file: pkg.module,
+                format: 'es',
+            },
+        ],
+        plugins: [
+            resolve({
+                extensions,
+            }),
+            commonjs(),
+            typescript(),
+            terser(),
+        ],
+        external: ['react'],
+    },
+];
