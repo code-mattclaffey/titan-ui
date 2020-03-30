6288b052b3c81f5018ecaa606d86c83915ce3adb
Matt Claffey <mattclaffey93@gmail.com>
2020-03-30 15:01:16 +0100

    chore: get rid of babel

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,9 @@ module.export = {
     roots: ['<rootDir>/src'],
     transform: {
         '\\.(ts|tsx)?$': 'ts-jest',
-        '\\.(js|jsx)?$': 'babel-jest',
     },
-    testMatch: ['<rootDir>/src/**/>(*.)test.{js, jsx, ts, tsx}'], // finds test
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node', 'tsx', 'ts'],
+    testMatch: ['<rootDir>/src/**/>(*.)test.{ts, tsx}'], // finds test
+    moduleFileExtensions: ['json', 'node', 'tsx', 'ts'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     setupFilesAfterEnv: [
         '@testing-library/jest-dom/extend-expect',
