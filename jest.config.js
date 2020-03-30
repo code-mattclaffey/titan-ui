a5b8d4a8f2de3a42ebf50723a33eaf679ddbe105
Matt Claffey <mattclaffey93@gmail.com>
2020-03-30 16:44:16 +0100

    fix: module.exports

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
-module.export = {
+module.exports = {
     roots: ['<rootDir>/src'],
     transform: {
         '\\.(ts|tsx)?$': 'ts-jest',
     },
-    testMatch: ['<rootDir>/src/**/>(*.)test.{ts, tsx}'], // finds test
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    setupFilesAfterEnv: [
-        '@testing-library/jest-dom/extend-expect',
-        '@testing-library/react/cleanup-after-each',
-    ],
+    testRegex: '.*.test\\.(ts|tsx)$',
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
